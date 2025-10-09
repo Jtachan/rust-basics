14a3455d0234697c69b68e0bb8412dcc0cce9f5b
Jtachan <76098522+Jtachan@users.noreply.github.com>
2025-10-09 10:46:25 +0200

    Comments & fix implemented

--- scripts/book_broken_scripts.rs
+++ scripts/book_broken_scripts.rs
@@ -3,19 +3,36 @@
     which will NOT compile.
     This file is an extra help to test the non-compiling cases and learn better.
  */
-
+#![allow(unused)]
 
 fn add_big_strings(dst: &mut Vec<String>, src: &[String]) {
-    // 'dst' has read & write permissions (borrow)
+    // The next commented code is the one that won't allow to compile:
 
-    let largest: &String =
-        // 'dst' returns the write permission
-        dst.iter().max_by_key(|s| s.len()).unwrap();
+    // let largest: &String =
+    //     // 'dst' borrows ownership permission to 'largest'
+    //     dst.iter().max_by_key(|s| s.len()).unwrap();
+    //
+    // for s in src {
+    //     if s.len() > largest.len() {
+    //         // 'dst' requires of write permissions
+    //         dst.push(s.clone());
+    //     }
+    // }
 
+    // FIX 1: use .clone() to define largest and not have permission conflicts
+    //      let largest = dst.[...].unwrap().clone();
+    // this will however may create performance issues.
+
+    // FIX 2: As the code only needs the length of 'largest', just save that value.
+    let largest_len: usize = dst.iter().max_by_key(|s| s.len()).unwrap().len();
     for s in src {
-        if s.len() > largest.len() {
+        if s.len() > largest_len {
             // 'dst' requires of write permissions
             dst.push(s.clone());
         }
     }
+}
+
+fn main() {
+    println!("Hi!");
 }
