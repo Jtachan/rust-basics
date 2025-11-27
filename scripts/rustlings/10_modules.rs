be9135b6d6fa8d26115d5fd83ffec682d53bcb70
Jaime Gonzalez <76098522+Jtachan@users.noreply.github.com>
2025-11-27 15:31:41 +0100

    R10 - exercise 2

--- scripts/rustlings/10_modules.rs
+++ scripts/rustlings/10_modules.rs
@@ -11,6 +11,30 @@ mod sausage_factory {
     }
 }
 
+/* Exercise 2: make use of the keywords 'use' and 'as' */
+mod delicious_snacks {
+    pub use self::fruits::PEAR as fruit;
+    pub use self::veggies::CUCUMBER as veggie;
+
+    // Unused items have been commented to avoid compiler warnings
+    mod fruits {
+        pub const PEAR: &str = "Pear";
+        // pub const APPLE: &str = "Apple";
+    }
+
+    mod veggies {
+        pub const CUCUMBER: &str = "Cucumber";
+        // pub const CARROT: &str = "Carrot";
+    }
+}
+
 fn main() {
+    println!("\nExercise 1:");
     sausage_factory::make_sausage();
+
+    println!("\nExercise 2:");
+    println!("favourite snacks: {} and {}",
+        delicious_snacks::fruit,
+        delicious_snacks::veggie,
+    )
 }
