511f9b42cca3694a59703ddf7b4065e633fa422a
Jaime Gonzalez <76098522+Jtachan@users.noreply.github.com>
2025-12-01 13:24:49 +0100

    R11: E3

--- scripts/rustlings/11_hashmaps.rs
+++ scripts/rustlings/11_hashmaps.rs
@@ -42,6 +42,52 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
     }
 }
 
+/*
+   Exercise 3: Build a score table for soccer (football) matches.
+*/
+
+#[derive(Default, Debug)]
+struct TeamScores {
+    goals_scored: u8,
+    goals_conceded: u8,
+}
+
+fn build_scores_table(result: &str) -> HashMap<&str, TeamScores> {
+    let mut scores = HashMap::<&str, TeamScores>::new();
+
+    for line in result.lines() {
+        let mut split_iterator = line.split(',');
+        // NOTE: We use `unwrap` because we didn't deal with error handling yet.
+        let team_1_name = split_iterator.next().unwrap();
+        let team_2_name = split_iterator.next().unwrap();
+        let team_1_score: u8 = split_iterator.next().unwrap().parse().unwrap();
+        let team_2_score: u8 = split_iterator.next().unwrap().parse().unwrap();
+
+        // Populate the score table
+        let team_record = scores.entry(team_1_name).or_insert(TeamScores {
+            goals_scored: 0,
+            goals_conceded: 0,
+        });
+        team_record.goals_scored += team_1_score;
+        team_record.goals_conceded += team_2_score;
+
+        let team_record = scores.entry(team_2_name).or_insert(TeamScores {
+            goals_scored: 0,
+            goals_conceded: 0,
+        });
+        team_record.goals_scored += team_2_score;
+        team_record.goals_conceded += team_1_score;
+    }
+
+    scores
+}
+
+const RESULTS: &str = "England,France,4,2
+France,Italy,3,1
+Poland,Spain,2,0
+Germany,England,2,1
+England,Spain,1,0";
+
 fn main() {
     // Exercise 1
     let basket = basket_template();
@@ -73,6 +119,22 @@ fn main() {
         };
         assert!(*amount > 0);
     }
+
+    // Exercise 3
+    let scores = build_scores_table(RESULTS);
+    dbg!(&scores);
+
+    assert!(["England", "France", "Germany", "Italy", "Poland", "Spain"]
+        .iter()
+        .all(|team_name| scores.contains_key(team_name)));
+
+    let team = scores.get("England").unwrap();
+    assert_eq!(team.goals_scored, 6);
+    assert_eq!(team.goals_conceded, 4);
+
+    let team = scores.get("Spain").unwrap();
+    assert_eq!(team.goals_scored, 0);
+    assert_eq!(team.goals_conceded, 3);
 
     println!("All tests passed!");
 }
