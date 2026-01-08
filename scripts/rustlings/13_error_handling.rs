f34f38bd15972ed7d2de79b288e121a72dad81d1
Jaime Gonzalez <76098522+Jtachan@users.noreply.github.com>
2026-01-08 15:43:27 +0100

    Rustlings 13 Exercise 2

--- scripts/rustlings/13_error_handling.rs
+++ scripts/rustlings/13_error_handling.rs
@@ -1,3 +1,5 @@
+use std::num::{IntErrorKind, ParseIntError};
+
 // Exercise 1: Create a function that returns a Result type depending on the input provided.
 fn generate_nametag_text(name: String) -> Result<String, String> {
     if name.is_empty() {
@@ -5,6 +7,30 @@ fn generate_nametag_text(name: String) -> Result<String, String> {
     } else {
         Ok(format!("Hi! My name is {name}"))
     }
+}
+
+/*
+   Exercise 2:
+       Asume a program where the user inputs a quantity (as a string) and the total cost to
+       buy that quantity is calculated by the function 'total_cost'.
+
+       Code a routine to return a `ParseIntError` when the input string does not contain only
+       numbers. No math operation must be performed in case of any error.
+       There is two ways to solve this exercise.
+*/
+fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
+    let process_fee = 1;
+    let cost_per_item = 5;
+
+    // The `?` operator works as a 'match' returning the Err if there is any.
+    // The following line is analogous to the commented code.
+    let qty = item_quantity.parse::<i32>()?;
+    // let qty = match item_quantity.parse::<i32>() {
+    //     Ok(v) => v,
+    //     Err(e) => return Err(e),
+    // };
+
+    Ok(qty * cost_per_item + process_fee)
 }
 
 fn main() {
@@ -18,6 +44,13 @@ fn main() {
             .as_ref()
             .map_err(|e| e.as_str()),
         Err("Empty names aren't allowed"),
+    );
+
+    // Exercise 2
+    assert_eq!(total_cost("34"), Ok(171));
+    assert_eq!(
+        total_cost("beep boop").unwrap_err().kind(),
+        &IntErrorKind::InvalidDigit
     );
 
     println!("All tests passed!");
