33fb80581905a90369af71345b7e3e7961b5fa28
Jatchan <jgonzg@proton.me>
2026-02-27 10:54:09 +0100

    minigrep: Improved modularity and error handling

--- crates/minigrep/src/main.rs
+++ crates/minigrep/src/main.rs
@@ -7,16 +7,46 @@
    CLI arguments
        By running `cargo run --` anything provided after `--` is considered an argument.
 */
-use std::env;
-use std::fs;
+use std::error::Error;
+use std::{env, fs, process};
 
 fn main() {
     // The first argument is the path to the executable (target folder).
     let args: Vec<String> = env::args().collect();
-    let file_path = &args[1];
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    println!("Reading '{file_path}'...\n");
-    let contents = fs::read_to_string(file_path).expect("Unable to read the file");
+    println!("Searching for '{}' in the file contents.", config.query);
+    println!("Reading '{}'...\n", config.file_path);
 
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(2);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
     println!("Read content:\n-------------\n{contents}");
+    Ok(())
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
 }
