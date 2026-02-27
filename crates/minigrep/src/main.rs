4d8ea42242c07a8034e43788f97881137944f561
Jatchan <jgonzg@proton.me>
2026-02-27 09:38:06 +0100

    minigrep: Successfully reading a file

--- crates/minigrep/src/main.rs
+++ crates/minigrep/src/main.rs
@@ -1,16 +1,22 @@
 /*
-    MINIGREP: An I/O Project from the Rust Book (chapter 12)
-        https://doc.rust-lang.org/book/ch12-00-an-io-project.html
+   MINIGREP: An I/O Project from the Rust Book (chapter 12)
+       https://doc.rust-lang.org/book/ch12-00-an-io-project.html
 
-    Features
-    --------
-    CLI arguments
-        By running `cargo run --` anything provided after `--` is considered an argument.
- */
+   Features
+   --------
+   CLI arguments
+       By running `cargo run --` anything provided after `--` is considered an argument.
+*/
 use std::env;
+use std::fs;
 
 fn main() {
     // The first argument is the path to the executable (target folder).
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+    let file_path = &args[1];
+
+    println!("Reading '{file_path}'...\n");
+    let contents = fs::read_to_string(file_path).expect("Unable to read the file");
+
+    println!("Read content:\n-------------\n{contents}");
 }
