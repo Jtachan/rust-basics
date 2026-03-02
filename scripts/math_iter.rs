940916e00ebdfe8794dcaf306964a75f4c13a967
Jatchan <jgonzg@proton.me>
2026-03-02 11:37:41 +0100

    Scripts: Math - fibonacci

--- scripts/math_iter.rs
+++ scripts/math_iter.rs
@@ -18,8 +18,32 @@ pub fn factorial(num: u64) -> u64 {
     (2..=num).fold(1, |acc, x| acc * x)
 }
 
+/// Defines the Fibonacci Series with length `n`.
+///
+/// # Examples
+/// ```rust
+/// assert_eq!(fibonacci(0), []);
+/// assert_eq!(fibonacci(3), [0, 1, 1]);
+///
+/// let series: Vec<u64> = fibonacci(17);
+/// assert_eq!(series.len(), 17);
+/// assert_eq!(series.pop(), 987);
+/// ```
+pub fn fibonacci(n: usize) -> Vec<u64> {
+    let mut sequence: Vec<u64> = Vec::new();
+    for i in 1..=n {
+        if i < 3 {
+            sequence.push((i - 1) as u64);
+        } else {
+            sequence.push(sequence[i - 2] + sequence[i - 3]);
+        }
+    }
+    sequence
+}
+
 #[cfg(test)]
 mod tests {
+    use std::intrinsics::assert_inhabited;
     use super::*;
 
     #[test]
@@ -28,5 +52,22 @@ mod tests {
         assert_eq!(factorial(1), 1);
         assert_eq!(factorial(2), 2);
         assert_eq!(factorial(5), 120);
+    }
+
+    #[test]
+    fn fib_series_initialization() {
+        assert_eq!(fibonacci(0), []);
+        assert_eq!(fibonacci(2), [0, 1]);
+        assert_eq!()
+    }
+
+    #[test]
+    fn fib_long_series() {
+        let mut series = fibonacci(17);
+        assert_eq!(series.len(), 17);
+        assert_eq!(series.pop(), Some(987));
+        let mut series = fibonacci(17);
+        assert_eq!(series.len(), 17);
+        assert_eq!(series.pop(), Some(987));
     }
 }
