f90b291c2127891fcee2b8f5e75f6964bc0a43f3
Jatchan <jgonzg@proton.me>
2026-03-03 13:11:15 +0100

    Scripts: Improved math iterators over fibonacci

--- scripts/math_iter.rs
+++ scripts/math_iter.rs
@@ -7,6 +7,8 @@
     The documentation comments use markdown for formatting.
  */
 
+use std::iter;
+
 /// Obtains the factorial of the number `num` (unsigned).
 /// The factorial of any number is defined as:
 ///
@@ -15,6 +17,16 @@
 ///
 /// For more information, see <https://en.wikipedia.org/wiki/Factorial>
 pub fn factorial(num: u64) -> u64 {
+
+    // INTUITIVE SOLUTION: for-loop
+    // -------------------
+    // let mut result = 1;
+    // for n in 2..=num {
+    //     result *= n;
+    // }
+    // result
+
+    // SOLUTION: Using `fold` -> https://doc.rust-lang.org/std/iter/trait.Iterator.html#method.fold
     (2..=num).fold(1, |acc, x| acc * x)
 }
 
@@ -30,20 +42,28 @@ pub fn factorial(num: u64) -> u64 {
 /// assert_eq!(series.pop(), 987);
 /// ```
 pub fn fibonacci(n: usize) -> Vec<u64> {
-    let mut sequence: Vec<u64> = Vec::new();
-    for i in 1..=n {
-        if i < 3 {
-            sequence.push((i - 1) as u64);
-        } else {
-            sequence.push(sequence[i - 2] + sequence[i - 3]);
-        }
-    }
-    sequence
+
+    // INTUITIVE SOLUTION: Using a for-loop
+    // ------------------------------------
+    // let mut sequence: Vec<u64> = Vec::new();
+    // for i in 1..=n {
+    //     if i < 3 {
+    //         sequence.push((i - 1) as u64);
+    //     } else {
+    //         sequence.push(sequence[i - 2] + sequence[i - 3]);
+    //     }
+    // }
+    // sequence
+
+    // SOLUTION: using iter::successors -> https://doc.rust-lang.org/std/iter/fn.successors.html
+    iter::successors(Some((0u64, 1u64)), |(a, b)| Some((*b, a + b)))
+        .map(|(a, _)| a)
+        .take(n)
+        .collect()
 }
 
 #[cfg(test)]
 mod tests {
-    use std::intrinsics::assert_inhabited;
     use super::*;
 
     #[test]
@@ -58,14 +78,10 @@ mod tests {
     fn fib_series_initialization() {
         assert_eq!(fibonacci(0), []);
         assert_eq!(fibonacci(2), [0, 1]);
-        assert_eq!()
     }
 
     #[test]
     fn fib_long_series() {
-        let mut series = fibonacci(17);
-        assert_eq!(series.len(), 17);
-        assert_eq!(series.pop(), Some(987));
         let mut series = fibonacci(17);
         assert_eq!(series.len(), 17);
         assert_eq!(series.pop(), Some(987));
