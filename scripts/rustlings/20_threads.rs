d547b64b0af4776e9e30dee98b87b69078e45ee4
Jatchan <jgonzg@proton.me>
2026-03-10 11:12:48 +0100

    Rustlings: Arc exercise (within threads and not smart pointers)

--- scripts/rustlings/20_threads.rs
+++ scripts/rustlings/20_threads.rs
@@ -1,3 +1,4 @@
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::{Duration, Instant};
 
@@ -31,6 +32,34 @@ fn exercise_1() {
     );
 }
 
+// Exercise Arc pointers
+fn atomic_rec_counter() {
+    let numbers: Vec<_> = (0..100u32).collect();
+    let shared_numbers = Arc::new(numbers);
+    let mut join_handles = Vec::new();
+
+    for offset in 0..8 {
+        let child_numbers = Arc::clone(&shared_numbers);
+
+        let handle = thread::spawn(move || {
+            let sum: u32 = child_numbers.iter().filter(|&&n| n % 8 == offset).sum();
+            println!("Sum of offset {offset} is {sum}");
+        });
+
+        join_handles.push(handle);
+    }
+
+    for handle in join_handles.into_iter() {
+        handle.join().unwrap();
+    }
+}
+
 fn main() {
+    println!("\nRunning exercise 1...");
     exercise_1();
+
+    println!("\nRunning Arc exercise...");
+    atomic_rec_counter();
+
+    println!("\nAll exercises passed!");
 }
