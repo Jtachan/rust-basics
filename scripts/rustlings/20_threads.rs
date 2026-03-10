799e7d7edf3ce3e74aadf10f2faf2f74f4b8279a
Jatchan <jgonzg@proton.me>
2026-03-10 11:20:53 +0100

    Rustlings 20 - Threads exercise 2

--- scripts/rustlings/20_threads.rs
+++ scripts/rustlings/20_threads.rs
@@ -54,12 +54,42 @@ fn atomic_rec_counter() {
     }
 }
 
+// Exercise 2: Concurrency on shared data
+struct JobStatus {
+    jobs_done: u32,
+}
+
+fn exercise_2() {
+    let status = Arc::new(Mutex::new(JobStatus { jobs_done: 0 }));
+    let mut handles = Vec::new();
+
+    for _ in 0..10 {
+        let status_shared = Arc::clone(&status);
+
+        let handle = thread::spawn(move || {
+            thread::sleep(Duration::from_millis(250));
+            status_shared.lock().unwrap().jobs_done += 1;
+        });
+
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Jobs done: {}", status.lock().unwrap().jobs_done);
+}
+
 fn main() {
     println!("\nRunning exercise 1...");
     exercise_1();
 
     println!("\nRunning Arc exercise...");
     atomic_rec_counter();
+
+    println!("\nRunning exercise 2...");
+    exercise_2();
 
     println!("\nAll exercises passed!");
 }
